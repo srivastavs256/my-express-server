967b60b756d93dfa82cb313e209423b4fa12381e
Srivastavs256 <srivastavs256@gmail.com>
2026-08-15 11:38:17 +0530

    Sahi syntax aur updated code

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const express = require('express'); const app = express(); app.get('/', (req, res) = running!')); app.listen(3000, () = is live on port 3000')); 
+const express = require('express');
+const app = express();
+const PORT =300;
+
+app.use((req,res,) =>{
+    req.send("Hello this is my first  World");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is up and runing on port ${PORT}!  ready to handle the file request`);
+});

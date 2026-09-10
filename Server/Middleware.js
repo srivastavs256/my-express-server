7a51c242fa7608ed0abbd459c36111dab6d91ef5
Srivastavs256 <srivastavs256@gmail.com>
2026-09-11 01:24:25 +0530

    Middleware function

---
+++ Server/Middleware.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+
+app.use((req, res, next) => {
+    
+    console.log(`${req.method} request made to ${req.url}`);
+  
+    next(); 
+});
+
+
+app.get('/products', (req, res) => {
+    res.send("Here is the list of all products.");
+});
+
+app.post('/products', (req, res) => {
+    res.send("A new product has been added.");
+});
+
+app.get('/categories', (req, res) => {
+    res.send("Here is the list of all categories.");
+});
+
+app.post('/categories', (req, res) => {
+    res.send("A new category has been created.");
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});

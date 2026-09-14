25fa205ad4cdfa54379d988356b07bacb6427809
Srivastavs256 <srivastavs256@gmail.com>
2026-09-14 21:45:31 +0530

    Unknown Route Server Added

---
+++ Server/UnknownRoute.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/products', (req, res) => {
+    res.send('Here is the list of all products.');
+});
+
+app.post('/products', (req, res) => {
+    res.send('A new product has been added.');
+});
+
+app.get('/categories', (req, res) => {
+    res.send('Here is the list of all categories.');
+});
+
+app.post('/categories', (req, res) => {
+    res.send('A new category has been created.');
+});
+
+app.all(/(.*)/, (req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running smoothly on http://localhost:${PORT}`);
+});
